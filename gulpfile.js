f67c0b6df7b6df95fdf5449ac6ea67d343ab5163
Robin Rendle <robin@erskinedesign.com>
2015-01-26 10:56:02 +0000

    initial gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,44 @@
+// Default gulpfile tasks for strtr
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
     minifyCSS = require('gulp-minify-css'),
     rename = require('gulp-rename'),
-    eslint = require('gulp-eslint'),
-    autoprefixer = require('gulp-autoprefixer');
+    prefix = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    size = require('gulp-size');
+
+
+// Compiles scss into the build/css dir
+gulp.task('sass', function(){
+    gulp.src('./sass/project.scss')
+        .pipe(watch(function(files){
+            return files.pipe(sass())
+                .pipe(prefix())
+                .pipe(size({gzip: true, showFiles: true, title:'unminified project.css'}))
+                .pipe(gulp.dest('./build/css'))
+        }));
+});
+
+// Minifies CSS
+gulp.task('minify', function(){
+    gulp.src('./build/css/project.scss')
+        .pipe(size({gzip: true, showFiles: true, title:'minified project.css'}))
+        .pipe(minifyCSS())
+        .pipe(size({gzip: true, showFiles: true, title:'minified colors.css'}))
+        .pipe(rename('project.min.css'))
+        .pipe(gulp.dest('./css/'));
+});
+
+// Autoprefixes on minified CSS
+gulp.task('prefix', function(){
+    gulp.src('./build/css/*.css')
+        .pipe(prefix())
+        .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('default', function(){
+    gulp.watch('./sass/project.scss', ['sass']);
+});
