e67635b6f66a1c47637ebdbf27b2ab13ba8cf6e2
Robin Rendle <desk@robinrendle.com>
2015-10-23 13:01:03 +0100

    added fonts, styles, markup and validation

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,89 @@
 var gulp = require('gulp'),
+    clean = require("gulp-clean"),
     sass = require('gulp-sass'),
-    minifyCSS = require('gulp-minify-css'),
     rename = require('gulp-rename'),
-    prefix = require('gulp-autoprefixer'),
+    autoprefixer = require('gulp-autoprefixer'),
     watch = require('gulp-watch'),
     size = require('gulp-size'),
     imageop = require('gulp-image-optimization'),
-    browserSync = require('browser-sync').create(),
-    reload = browserSync.reload;
+    browserSync = require("browser-sync").create(),
+    concat = require("gulp-concat"),
+    // IMAGES
+    // SCRIPTS
+    uglify = require("gulp-uglify"),
+    jshint = require("gulp-jshint"),
+    // PATHS
+    paths = {
+        styles: {
+            src: "src/sass/project.scss",
+            dest: "build/css",
+            watch: "src/sass/**/*.*",
+        },
+        scripts: {
+            libs: [
+                "src/js/lib/jquery.js",
+                "src/js/lib/jquery.validation.js"
+            ],
+            src: [
+                "src/js/app.js"
+            ],
+            dest: "build/js/",
+            watch: "src/js/**/*.*",
+        },
+        images: {
+            src: [
+                "src/img/*.*",
+            ],
+            dest: "build/img"
+        },
+        watch: [
+            "index.html",
+        ],
+    };
 
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
+// Static Server
+gulp.task('browser-sync', ['styles', 'scripts'], function() {
+    browserSync.init([paths.watch], {
         server: "./"
     });
-    gulp.watch(['src/sass/*.scss', 'src/sass/**/*.scss' ], ['sass']);
-    gulp.watch("index.html").on('change', reload);
+
+    gulp.watch(paths.styles.watch, ['styles']);
+    gulp.watch(paths.scripts.watch, ['scripts']);
+    gulp.watch(paths.watch).on('change', browserSync.reload);
 });
 
-// Compiles scss into the build/css dir
-gulp.task('sass', function(){
-    return gulp.src('./src/sass/*.scss')
-        .pipe(sass())
-        .pipe(prefix())
-        .pipe(gulp.dest('./build/css'))
-        .pipe(reload({stream: true}));
+gulp.task("styles", function (){
+    gulp.src(paths.styles.src)
+        .pipe(sass({
+            outputStyle: "compressed",
+        }))
+        .on("error", function(err){
+            browserSync.notify(err.message);
+        })
+        .pipe(autoprefixer("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(concat("project.min.css"))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 });
 
-// Image optimization from /img to /build/img
-gulp.task('images', function(cb){
-    gulp.src(['./src/img/**/*.png','./src/img/**/*.jpg','./src/img/**/*.gif','./src/img/**/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    }))
-    .pipe(size({gzip: true, showFiles: true}))
-    .pipe(gulp.dest('./build/img')).on('end', cb).on('error', cb);
+gulp.task("images", function(){
+    gulp.src(paths.images.src)
+        .on("error", function(err) { return err; })
+        .pipe(gulp.dest(paths.images.dest))
 });
 
-
-// Minifies CSS
-gulp.task('minify', function(){
-    gulp.src('./build/css/project.css')
-        .pipe(size({gzip: true, showFiles: true, title:'minified project.css'}))
-        .pipe(minifyCSS())
-        .pipe(rename('project.min.css'))
-        .pipe(gulp.dest('./build/css/'))
-        .pipe(size({gzip: true, showFiles: true, title:'minified project.css'}));
+gulp.task("scripts", function (){
+    gulp.src(paths.scripts.src)
+    gulp.src( paths.scripts.libs.concat(paths.scripts.src) )
+        .pipe(uglify())
+        .pipe(concat("app.min.js"))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
 });
 
-// Autoprefixes on minified CSS
-gulp.task('prefix', function(){
-    gulp.src('./build/css/*.css')
-        .pipe(prefix())
-        .pipe(gulp.dest('./build/css'));
+gulp.task("clean", function(){
+    gulp.src([paths.styles.dest, paths.scripts.dest], {read: false})
+        .pipe(clean());
 });
 
-// Tasks for production
-gulp.task('build', ['images', 'sass', 'minify']);
-
-// Default tasks
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['styles', 'scripts', 'images', 'browser-sync']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,25 @@
 // =======================================================================
-// APP 
+// APP
 // =======================================================================
 
+(function(){
+
+    var App = {
+        init: function(){
+            console.log("application ready");
+
+            $("form").validate(function(){});
+
+            $("input").focus(function(){
+                $("form").addClass("visible");
+            });
+        }
+    };
+
+    if(window.addEventListener && document.querySelector) {
+        var body = document.documentElement;
+        body.classList.remove("no-js");
+        body.classList.add("js");
+        App.init();
+    }
+})();
